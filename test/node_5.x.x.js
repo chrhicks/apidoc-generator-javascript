ab3a5fb4ec61d4f9c7b062ed54561749002d59a8
Chris Hicks <chicks@flow.io>
2016-02-19 15:52:11 -0500

    basic 5.x.x client generation

--- test/node_5.x.x.js
+++ test/node_5.x.x.js
@@ -1,11 +1,31 @@
 /*global describe, it */
+import fs from 'fs';
 import expect from 'expect';
 
 import { generate } from '../lib/generators/5.x.x';
 
+const CLIENT_FILE = 'apidoc-test-light-0.0.2.js';
+
+function generateClient() {
+  const json = fs.readFileSync(`${process.cwd()}/reference-api/api-light-service.json`).toString('utf-8');
+  const clientFiles = generate(JSON.parse(json));
+  // fs.createReadStream(`${process.cwd()}/public/templates/5.x.x/resources/utils.js`)
+  //   .pipe(fs.createWriteStream(`${process.cwd()}/test/dist/utils.js`));
+  clientFiles.forEach((file) => fs.writeFileSync(`${process.cwd()}/test/dist/${file.fileName}`, file.contents));
+}
+
+generateClient();
+
+const Client = require(`./dist/${CLIENT_FILE}`).default;
+const CLIENT_HOST = 'http://localhost:9020';
+const client = new Client(CLIENT_HOST);
+
 describe('node 5.x.x client', () => {
-  it('should pass', () => {
-    const generatedClient = generate({});
-    expect(generatedClient).toEqual('The 5.x.x client\n');
+  it('should have access to client', () => {
+    expect(client.getHost()).toEqual(CLIENT_HOST);
+  });
+
+  it('should have expected resource classes', () => {
+    expect(client.Flights).toBeA('object');
   });
 });
